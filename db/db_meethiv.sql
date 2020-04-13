795ef861d7d05c37f15f2cdd62a16cf159a303cb
iobasa <i_obasa@fanshaweonline.ca>
2020-04-12 23:17:09 -0400

    added database content

--- db/db_meethiv.sql
+++ db/db_meethiv.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 12, 2020 at 02:26 AM
+-- Generation Time: Apr 13, 2020 at 03:15 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.2.18
 
@@ -31,21 +31,26 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `tbl_content`;
 CREATE TABLE IF NOT EXISTS `tbl_content` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `page_id` int(11) NOT NULL,
-  `page_title` varchar(250) NOT NULL,
-  `description` text NOT NULL,
-  `title` varchar(300) NOT NULL,
+  `myth` varchar(300) NOT NULL,
+  `fact_title` varchar(300) NOT NULL,
+  `fact_description` text NOT NULL,
+  `link1` varchar(300) NOT NULL,
+  `link2` varchar(300) NOT NULL,
+  `video_id` varchar(300) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tbl_content`
 --
 
-INSERT INTO `tbl_content` (`id`, `page_id`, `page_title`, `description`, `title`) VALUES
-(1, 2, 'HIV 101', 'AIDS stands for Acquired Immunodeficiency Syndrome. Having HIV does not mean you have AIDS but when HIV is left untreated and completely breaks down your immune system over several years, it leads to HIV.', 'When left untreated, HIV leads to AIDS'),
-(2, 2, 'living with hiv', 'You might worry that you can never have a relationship, or sex, or that you won\'t be loved but it is absolutely possible to have all these things when you have HIV, it is important to remember that there is much more to you than HIV.', 'You can still love and have sex.'),
-(4, 3, 'fact', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'People with HIV can live like anyone as long as they undergo treatment.');
+INSERT INTO `tbl_content` (`id`, `myth`, `fact_title`, `fact_description`, `link1`, `link2`, `video_id`) VALUES
+(1, 'I can get HIV from hugging someone who has HIV.', 'You can only get HIV if infected bodily fluids get into your bloodstream through unprotected sex, pregnancy, and sharing needles.						', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.	', '/hiv-prevention', 'https://hivaidsconnection.ca/get-facts/get-tested', 'yFcwdeNt32A'),
+(2, 'I can treat HIV without any medical help.', 'If you are having sex, the only methods of HIV prevention are condoms or pre-exposure prophylaxis (PrEP).', 'No matter how much you wash your genitals, drink water, or pee, the chances of treating HIV through any kind of method besides medical help is zero. ', '/hiv-prevention', 'https://hivaidsconnection.ca/get-facts/get-tested', '85SATn36Rks'),
+(3, 'If you get HIV, your life is over.', 'With treatment, HIV-positive people can live a normal life span and live normal lives.\r\n	With treatment, they can be in serious and/or casual relationships and have babies without risking other people.				', 'With treatment, they can be in serious and/or casual relationships and have babies without risking other people.', '/hiv-living', 'https://hivaidsconnection.ca/get-facts/living-hivaidshep-c/hiv', 'mG75lUmEfb4'),
+(4, 'If I get infected fluid from an HIV+ person, I will get HIV.', 'Undetectable = Untransmittable.', 'HIV is not always passed on from someone living with HIV, especially if their bodies have undetectable levels of HIV. If you’re scared of the risk, it is important to take an HIV test every time.					', '/hiv-prevention', 'https://hivaidsconnection.ca/get-facts/get-tested', 'iPruHLfXP80'),
+(5, 'You can only get HIV if you’re gay.', 'HIV can be transmitted, no matter what your sexual orientation is.	', 'Like all illnesses, HIV doesn’t choose specific people to affect.					', '/hiv-prevention', 'https://hivaidsconnection.ca/get-facts/get-tested', 'Z1TCuLZO3i0'),
+(6, 'Having HIV means that person is dirty or sexually promiscuous.', 'The risk of HIV does not increase with the amount of sexual activity.', 'The risk of HIV increases when one does not practice safe sex, whether it be with one or more partners.\r\n(There’s also nothing wrong about being sexually active)', '/hiv-prevention', 'https://hivaidsconnection.ca/get-facts/get-tested', 'cBD6RCwFdoU');
 
 -- --------------------------------------------------------
 
--- js/components/Myth2Component.js
+++ js/components/Myth2Component.js
@@ -7,7 +7,7 @@ export default {
     <section>
 
     <section class="videoCont">
-    <iframe width="560" height="315" src="{{currentMediaDetails.video_id}}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/{{currentMediaDetails.video_id}}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     
                 </section>
 
